162eec4c6571bf450d1df969e98a4061d5e0b876
Ruyan Chen <ruyanche@usc.edu>
2013-04-25 13:38:32 -0700

    added initial game file and mainwindow

---
+++ game.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+#include <cmath>
+
+#include <QtGui/QApplication>
+#include "mw.h"
+
+using namespace std;
+
+/** @mainpage Starships (were meant to fly...), a game by Ruyan Chen for CSCI 102
+@section gameplay Gameplay
+
+
+
+*/
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+
+    //w.show();
+        
+    return a.exec();
+}
---
+++ mw.h
@@ -0,0 +1,15 @@
+#include <QMainWindow>
+
+class MainWindow : public QMainWindow{
+	Q_OBJECT
+	
+	public:
+	
+	public slots:
+	
+	protected:
+		void keyPressEvent( QKeyEvent *e );
+	
+	private:
+	
+};
